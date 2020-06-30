1ee859b5456c0f3cf44ea85cc4b7b5c2a1a0fb28
Lathe El Hafy <lathe.elhafy@gmail.com>
2020-06-29 21:48:39 -0400

    List adapter successfully added but not tested. Variables need to modified to reflect work that has been done. The workout will begin when the user starts the counter, the counter will then cycle through the list items executing each time varible that was set and run to the very end of the workout cycle.

--- app/src/main/java/com/gameoverdev/intervaltimer/Runner.java
+++ app/src/main/java/com/gameoverdev/intervaltimer/Runner.java
@@ -3,12 +3,21 @@ package com.gameoverdev.intervaltimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Runner extends AppCompatActivity {
 
+    String [] testArray = {"Something 1","something 2","something 3"};
+
+    private ArrayAdapter adapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_runner);
+        adapter  = new ArrayAdapter(this,R.layout.activity_runner,testArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
 }
