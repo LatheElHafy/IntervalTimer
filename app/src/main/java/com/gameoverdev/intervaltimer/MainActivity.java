9ebe1ddc8be26302286291a38d0488fddf77db1f
Lathe El Hafy <lathe.elhafy@gmail.com>
2020-06-29 21:43:02 -0400

    Adding list view

--- app/src/main/java/com/gameoverdev/intervaltimer/MainActivity.java
+++ app/src/main/java/com/gameoverdev/intervaltimer/MainActivity.java
@@ -2,13 +2,34 @@ package com.gameoverdev.intervaltimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextField = (TextView) findViewById(R.id.mTextField);
+
+        setTimer();
+    }
+
+    public void setTimer (){
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) { // on tick is the every movement of the second clock.
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000); // Where the field is updated.
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+        }.start();
+
     }
 }
---
+++ app/src/main/java/com/gameoverdev/intervaltimer/Runner.java
@@ -0,0 +1,14 @@
+package com.gameoverdev.intervaltimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Runner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_runner);
+    }
+}
